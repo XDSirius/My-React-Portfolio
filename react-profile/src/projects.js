b0cedf248204fc5f9db337cbd315eb94503487c3
Yasmin Castillo <yasmin_c@outlook.com>
2023-10-06 18:48:27 -0500

    added projects to portfolio

--- react-profile/src/projects.js
+++ react-profile/src/projects.js
@@ -1,16 +1,42 @@
 const projects = [
     {
         id: 1,
-        name: "First Project",
+        name: "Little Chef",
         url:"https://github.com/XDSirius/Little-Chef",
         imgURL:"src/assets/littlechef.png"
     },
     {
         id: 2,
-        name: "First Project",
-        url:"https://github.com/XDSirius/Little-Chef",
-        imgURL:"src/assets/littlechef.png"
-    }
+        name: "Your Network API",
+        url:"https://github.com/XDSirius/YourNetworkAPI",
+        imgURL:"src/assets/yournetworkapi.png"
+    },
+    {
+        id: 3,
+        name: "Employee Tracker Dashboard",
+        url:"https://github.com/XDSirius/Employee-Tracker-Dashboard",
+        imgURL:"src/assets/employeetracker.png"
+    },
+    {
+        id: 4,
+        name: "Netflix and Cocktails",
+        url:"https://github.com/XDSirius/Netflix-Cocktail-s",
+        imgURL:"https://user-images.githubusercontent.com/128574459/246696011-2b05934d-e40c-4c7c-a08a-726756504649.png"
+    },
+    {
+        id: 5,
+        name: "JavaScript Password Generator",
+        url:"https://github.com/XDSirius/JavaScript-Password-Generator",
+        imgURL:"src/assets/password.png"
+    },
+    {
+        id: 6,
+        name: "Work Day Scheduler",
+        url:"https://github.com/XDSirius/Work-Day-Scheduler",
+        imgURL:"src/assets/Workday.png"
+    },
+    
+
 ];
 
 export default projects;
