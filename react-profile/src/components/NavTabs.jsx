7b6408e12f0520ffa515982d83dddd544cc08a73
Yasmin Castillo <yasmin_c@outlook.com>
2023-10-06 17:55:42 -0500

    Revert "fixed pages"
    This reverts commit ad38048f7e30a8f0393443808c2b8274d82fb1d9.

--- react-profile/src/components/NavTabs.jsx
+++ react-profile/src/components/NavTabs.jsx
@@ -5,47 +5,45 @@ import React from 'react';
 function NavTabs({ currentPage, handlePageChange }) {
   return (
     <ul className="nav nav-tabs">
-      
-
       <li className="nav-item">
         <a
-          href="#aboutme"
-          onClick={() => handlePageChange('aboutme')}
+          href="#home"
+          onClick={() => handlePageChange('Home')}
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === 'aboutme' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
         >
-          About Me
+          Home
         </a>
       </li>
       <li className="nav-item">
         <a
-          href="#mywork"
-          onClick={() => handlePageChange('mywork')}
+          href="#about"
+          onClick={() => handlePageChange('About')}
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'mywork' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
         >
-          My Work
+          About
         </a>
       </li>
       <li className="nav-item">
         <a
-          href="#resume"
-          onClick={() => handlePageChange('resume')}
+          href="#blog"
+          onClick={() => handlePageChange('Blog')}
           // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'resume' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
         >
-          Resume
+          Blog
         </a>
       </li>
       <li className="nav-item">
         <a
-          href="#contactme"
-          onClick={() => handlePageChange('contactme')}
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
           // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'contactme' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
         >
-          Contact Me
+          Contact
         </a>
       </li>
     </ul>
--- react-profile/src/components/pages/Contact.jsx
+++ react-profile/src/components/pages/Contact.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Resume() {
+export default function Contact() {
   return (
     <div>
-      <h1>Resume</h1>
+      <h1>Contact Page</h1>
       <p>
         Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
         molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
