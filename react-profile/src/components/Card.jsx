ab4dc5ee2dcbdf9db62b24d35618fe1de171bc52
Yasmin Castillo <yasmin_c@outlook.com>
2023-10-06 17:59:36 -0500

    added card functionality

---
+++ react-profile/src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Cards({project}) {
+    return (
+    <div className="card">
+            <div>
+                <h1>Hello</h1>
+                <h3>{project.name}</h3>
+                <div className="img">
+                
+                    <a href={project.url}><img className="hello" src={project.imgURL}/></a>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Cards;
---
+++ react-profile/src/projects.js
@@ -0,0 +1,16 @@
+const projects = [
+    {
+        id: 1,
+        name: "First Project",
+        url:"https://github.com/XDSirius/Little-Chef",
+        imgURL:"/imgs/littlechef.png"
+    },
+    {
+        id: 2,
+        name: "First Project",
+        url:"https://github.com/XDSirius/Little-Chef",
+        imgURL:"/imgs/littlechef.png"
+    }
+];
+
+export default projects;
